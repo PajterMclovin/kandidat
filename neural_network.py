3ce15abfe8414c21f4a837caa6bf5ce6550f4bc6
PajterMclovin <pajter_mclovin@hotmail.com>
2020-03-09 23:05:35 +0100

    comments

--- neural_network.py
+++ neural_network.py
@@ -4,20 +4,18 @@
     the network structure.
 
 """
-
-#import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 # import numpy as np
-#import datetime as dt
+# import datetime as dt
 # import sys
 
-#import custom shit
-from loss_functions import Relative_loss
-from utils import read_data_npz_energy_angle#, subset
+#import custom stuff
+from loss_functions import Relative_loss        # custom keras.losses.Loss-object
+from utils import read_data_npz_energy_angle    # to load data
 
 
-#IMPORTANT PARAMETERS (from last year)
+#IMPORTANT PARAMETERS (stolen from last years rep)
 #npz_file_name = sys.argv[1]             #String with name of the generated data
 npz_file_name = 'XB_mixed_data_1-2_653348.npz'
 no_iter = 10000                         #Number of training iterations
@@ -28,21 +26,14 @@ E_offset = 0                            #Offset value in relative cost function
 theta_offset = 0
 phi_offset = 0
 dropout_rate = 0                        #Rate of dropout, [0,1] (0 = dropout off)
-used_data_portion = 1.0                 #The used amount data, [0,1]                                
-use_relative_loss = 0                   #0 = NOT USE relative loss function, 1 = USE -||-                    
+used_data_portion = 1.0                 #The used amount data, [0,1]                                                   
 save_folder_name = 'TEST_NAME'          #Name of the directory with all the saved data
 
-#OTHER PARAMETERS
 training_portion = 0.8                  #The portion of the data used for training, [0,1]
 alpha = 0.001                           #Slope for negative values for Leaky ReLu
 no_epochs = 1                           #Number of times to go through training data
 batch_size = 300                        #The training batch size
-loss_size = 300                         #Batch size for calculating loss during training
-no_bins = 50                            #Number of bins to calculate standard deviation and error
 beta = 0                                #Weighting factor for the weight decay loss (do not use = 0)
-learning_rate = 1e-4                    #The learning rate
-rel_beta = 0.7                          #The speed of the relativistic beam (only used for relativistic corrections)
-message = 'Test_explanation'            #Text explaning the specific test in saved textfile
 
 '''
 Loading data from the npz file (may not be working when not using phi?)
@@ -58,35 +49,36 @@ no_input_nodes = len(train_data[0])                 #Width of input layer (162)
 #max_E = np.round(np.amax(eval_labels),1)            #Maximum correct energy
 #max_energy_angle_values = [max_E, np.pi, 2*np.pi]
 
-weight_decay_loss = 1
-#create custom loss function for Keras module
+weight_decay_loss = 1   #what's this for?
 
 
 
-# NETWORK DESIGN
+
+# -----------------NETWORK DESIGN-----------------------
+
+## define layers with Layer-objects (keras)
 inputs = keras.Input(shape=(no_input_nodes,))
 x = layers.Dense(128, activation='softmax')(inputs)
 for i in range(9):
     x = layers.Dense(128, activation='softmax')(x)
 outputs = layers.Dense(no_output, activation='softmax')(x)
 
+## create the network
 model = keras.Model(inputs, outputs)
 
-#something to keep track of training...? Can easily be saved to keep track of all tests
-# callbacks = [
-#     # Write TensorBoard logs to `./logs` directory
-#     keras.callbacks.TensorBoard(log_dir='./log/{}'.format(dt.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")), write_images=True)
-# ]
-
+## select loss function with Loss-object (keras)
 num_splits = int(no_output/3)
 loss_func = Relative_loss(num_splits,
                           lam_E, lam_theta, lam_phi,
                           E_offset, theta_offset, phi_offset,
                           weight_decay_loss, beta)
 
-
+## compile model
 model.compile(optimizer=keras.optimizers.Adam(),
               loss=loss_func,
               metrics=['accuracy'])
 
+## train the network
 model.fit(train_data, train_labels, epochs=10, batch_size=batch_size)
+
+## evaluate, save etc.
