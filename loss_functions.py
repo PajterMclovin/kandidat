31cdfa77061f48b3f799b6730654e33189bee5f3
DavidRinman <61792198+DavidRinman@users.noreply.github.com>
2020-03-24 15:09:57 +0100

    Update loss_functions.py
    la till en vector_loss som använder samma indata som 2019 (energi, vinklar). Inverkan av vinklarna på kostnaden regleras med LAMBDA_VECTOR

--- loss_functions.py
+++ loss_functions.py
@@ -7,6 +7,7 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
+import tensorflow.math as m
 
 
 # LOSS FUNCTION PARAMETERS
@@ -14,7 +15,7 @@ LAMBDA_ENERGY = 1
 OFFSET_ENERGY = 0.1     #don't divide with 0! used in relative loss
 LAMBDA_THETA = 1
 LAMBDA_PHI = 1
-
+LAMBDA_VECTOR = 1
 
 """
     OBS. why calculate the loss for all permutations? Why not train the network to
@@ -35,11 +36,11 @@ def relative_loss(y, y_):
     """
     energy, energy_ = y[::,0::3], y_[::,0::3]
     theta, theta_ = y[::,1::3], y_[::,1::3]
-    phi, phi_ = y[::,2::3], y_[::,2::3]
+    phi, phi_ = y[::,2::3], y[::,2::3]
     
     loss_energy = LAMBDA_ENERGY*K.square(tf.divide(energy-energy_, energy_+OFFSET_ENERGY))
     loss_theta = LAMBDA_THETA*K.square(theta-theta_)
-    loss_phi = LAMBDA_PHI*K.square(tf.math.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
+    loss_phi = LAMBDA_PHI*K.square(m.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
     return K.mean(loss_energy + loss_theta + loss_phi)
 
 
@@ -56,10 +57,32 @@ def absolute_loss(y, y_):
     """
     energy, energy_ = y[::,0::3], y_[::,0::3]
     theta, theta_ = y[::,1::3], y_[::,1::3]
-    phi, phi_ = y[::,2::3], y_[::,2::3]
+    phi, phi_ = y[::,2::3], y[::,2::3]
     
     loss_energy = LAMBDA_ENERGY*K.square(energy-energy_)
     loss_theta = LAMBDA_THETA*K.square(theta-theta_)
-    loss_phi = LAMBDA_PHI*K.square(tf.math.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
+    loss_phi = LAMBDA_PHI*K.square(m.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
     return K.mean(loss_energy + loss_theta + loss_phi)
     
+def vector_loss(y, y_):
+     
+     """ 
+     vektorbaserad kostnadsfunktion, förmodar att indata är i samma format som 2019 (energi, vinklar)
+     """
+     
+     energy, energy_ = y[::,0::3], y_[::,0::3]
+     theta, theta_ = y[::,1::3], y_[::,1::3]
+     phi, phi_ = y[::,2::3], y[::,2::3]
+     
+     x,x_ = m.multiply(m.sin(theta), m.cos(phi)), m.multiply(m.sin(theta_), m.cos(phi_))
+     y,y_ = m.multiply(m.sin(theta), m.sin(phi)), m.multiply(m.sin(theta_), m.sin(phi_))
+     z,z_ = m.cos(theta), m.cos(theta_)
+     
+     r = [x,y,z]
+     r_ = [x_,y_,z_]
+     
+     loss_energy = LAMBDA_ENERGY*K.square(energy-energy_)
+     loss_angles = LAMBDA_VECTOR*(1-K.dot(r,r_))
+     
+     return K.mean(loss_energy + loss_angles)
+
