536b8306ab04123e49c01e0ab2c64d95789c2716
DavidRinman <61792198+DavidRinman@users.noreply.github.com>
2020-03-25 15:08:37 +0100

    fixed the shit

--- loss_functions.py
+++ loss_functions.py
@@ -7,7 +7,7 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
-import tensorflow.math as m
+
 
 
 # LOSS FUNCTION PARAMETERS
@@ -36,11 +36,11 @@ def relative_loss(y, y_):
     """
     energy, energy_ = y[::,0::3], y_[::,0::3]
     theta, theta_ = y[::,1::3], y_[::,1::3]
-    phi, phi_ = y[::,2::3], y[::,2::3]
+    phi, phi_ = y[::,2::3], y_[::,2::3]
     
     loss_energy = LAMBDA_ENERGY*K.square(tf.divide(energy-energy_, energy_+OFFSET_ENERGY))
     loss_theta = LAMBDA_THETA*K.square(theta-theta_)
-    loss_phi = LAMBDA_PHI*K.square(m.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
+    loss_phi = LAMBDA_PHI*K.square(tf.math.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
     return K.mean(loss_energy + loss_theta + loss_phi)
 
 
@@ -61,28 +61,30 @@ def absolute_loss(y, y_):
     
     loss_energy = LAMBDA_ENERGY*K.square(energy-energy_)
     loss_theta = LAMBDA_THETA*K.square(theta-theta_)
-    loss_phi = LAMBDA_PHI*K.square(m.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
+    loss_phi = LAMBDA_PHI*K.square(tf.math.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
     return K.mean(loss_energy + loss_theta + loss_phi)
-    
+
+  
 def vector_loss(y, y_):
      
      """ 
+
      vektorbaserad kostnadsfunktion, förmodar att indata är i samma format som 2019 (energi, vinklar)
      """
+
      
      energy, energy_ = y[::,0::3], y_[::,0::3]
      theta, theta_ = y[::,1::3], y_[::,1::3]
-     phi, phi_ = y[::,2::3], y[::,2::3]
+     phi, phi_ = y[::,2::3], y_[::,2::3]
      
-     x,x_ = m.multiply(m.sin(theta), m.cos(phi)), m.multiply(m.sin(theta_), m.cos(phi_))
-     y,y_ = m.multiply(m.sin(theta), m.sin(phi)), m.multiply(m.sin(theta_), m.sin(phi_))
-     z,z_ = m.cos(theta), m.cos(theta_)
+     x,x_ = tf.math.multiply(tf.math.sin(theta), tf.math.cos(phi)), tf.math.multiply(tf.math.sin(theta_), tf.math.cos(phi_))
+     y,y_ = tf.math.multiply(tf.math.sin(theta), tf.math.sin(phi)), tf.math.multiply(tf.math.sin(theta_), tf.math.sin(phi_))
+     z,z_ = tf.math.cos(theta), tf.math.cos(theta_)
      
      r = [x,y,z]
      r_ = [x_,y_,z_]
      
      loss_energy = LAMBDA_ENERGY*K.square(energy-energy_)
-     loss_angles = LAMBDA_VECTOR*(np.ones(r.shape)-K.dot(r,r_))
+     loss_angles = LAMBDA_VECTOR*(1-tf.math.multiply(x,x_)-tf.math.multiply(y,y_)-tf.math.multiply(z,z_))
      
      return K.mean(loss_energy + loss_angles)
-
