e10387f60bbf5fd61f86efd449f948c0b7e1d195
DavidRinman <61792198+DavidRinman@users.noreply.github.com>
2020-03-25 16:58:43 +0100

    Update loss_functions.py

--- loss_functions.py
+++ loss_functions.py
@@ -111,4 +111,26 @@ def absolute_loss(y, y_):
     loss_theta = LAMBDA_THETA*K.square(theta-theta_)
     loss_phi = LAMBDA_PHI*K.square(tf.math.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
     return K.mean(loss_energy + loss_theta + loss_phi)
-    
+
+  
+def vector_loss(y, y_):
+     
+     """ 
+
+     vektorbaserad kostnadsfunktion, förmodar att indata är i samma format som 2019 (energi, vinklar)
+     """
+
+     
+     energy, energy_ = y[::,0::3], y_[::,0::3]
+     theta, theta_ = y[::,1::3], y_[::,1::3]
+     phi, phi_ = y[::,2::3], y_[::,2::3]
+     
+     x,x_ = tf.math.multiply(tf.math.sin(theta), tf.math.cos(phi)), tf.math.multiply(tf.math.sin(theta_), tf.math.cos(phi_))
+     y,y_ = tf.math.multiply(tf.math.sin(theta), tf.math.sin(phi)), tf.math.multiply(tf.math.sin(theta_), tf.math.sin(phi_))
+     z,z_ = tf.math.cos(theta), tf.math.cos(theta_)
+
+     loss_energy = LAMBDA_ENERGY*K.square(energy-energy_)
+     loss_angles = LAMBDA_VECTOR*(1-tf.math.multiply(x,x_)-tf.math.multiply(y,y_)-tf.math.multiply(z,z_))
+     
+     return K.mean(loss_energy + loss_angles)
+
