9c798a83f6461ca53d561467cb505bb7e254a1a6
PajterMclovin <pajter_mclovin@hotmail.com>
2020-03-25 15:57:48 +0100

    correct update

--- loss_functions.py
+++ loss_functions.py
@@ -8,15 +8,22 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
 
-
+from utils import get_permutation_tensor, get_identity_tensor
 
 # LOSS FUNCTION PARAMETERS
 LAMBDA_ENERGY = 1
-OFFSET_ENERGY = 0.1     #don't divide with 0! used in relative loss
 LAMBDA_THETA = 1
 LAMBDA_PHI = 1
-LAMBDA_VECTOR = 1
 
+OFFSET_ENERGY = 0.1     #don't divide with 0! used in relative loss
+LAMBDA_VECTOR = 1       #used in vector loss
+
+
+"""
+    OBS. why calculate the loss for all permutations? Håkan thinks this would 
+    be a good idea.
+    
+"""
 def permutation_loss_wrapper(max_mult):
     """
     Wrapper function for permutation_loss
@@ -28,7 +35,7 @@ def permutation_loss_wrapper(max_mult):
     identity_tensor = get_identity_tensor(max_mult)
     def loss(y, y_):
         return permutation_loss(y, y_, permutation_tensor, identity_tensor)
-
+    return loss
 
 def permutation_loss(y, y_, permutation_tensor, identity_tensor):
     """
@@ -55,6 +62,7 @@ def permutation_loss(y, y_, permutation_tensor, identity_tensor):
     
     return K.min(loss_energy+loss_theta+loss_phi)
 
+
 def relative_loss(y, y_):
     """
     tensorflow.kereas.backend symbolic loss function with relative error in energy.
@@ -67,13 +75,21 @@ def relative_loss(y, y_):
         loss
     """
     energy, energy_ = y[::,0::3], y_[::,0::3]
-    theta, theta_ = y[::,1::3], y_[::,1::3]
-    phi, phi_ = y[::,2::3], y_[::,2::3]
+    theta, theta_ = y[::,1::3], y_[::,1::3]                 #zenith (0,pi)
+    phi, phi_ = y[::,2::3], y_[::,2::3]                      #azimuth (0,2pi)
     
-    loss_energy = LAMBDA_ENERGY*K.square(tf.divide(energy-energy_, energy_+OFFSET_ENERGY))
-    loss_theta = LAMBDA_THETA*K.square(theta-theta_)
-    loss_phi = LAMBDA_PHI*K.square(tf.math.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
-    return K.mean(loss_energy + loss_theta + loss_phi)
+    # loss_energy = K.mean(K.square(tf.divide(energy-energy_, energy_+OFFSET_ENERGY)))
+    
+    loss_energy = K.mean(K.square(energy-energy_))
+    # loss_theta = K.mean(K.square(theta-theta_))
+    # loss_phi = K.mean(K.square(phi-phi_))
+    
+    # loss_theta = K.mean(tf.abs(tf.atan2(tf.sin(theta-theta_), tf.cos(theta-theta_))))
+    # loss_phi = K.mean(tf.abs(tf.atan2(tf.sin(phi-phi_), tf.cos(phi-phi_))))
+    
+    loss_theta = K.mean(1-K.cos(theta-theta_))
+    loss_phi = K.mean(1-K.cos(phi-phi_))
+    return LAMBDA_ENERGY*loss_energy + LAMBDA_THETA*loss_theta + LAMBDA_PHI*loss_phi
 
 
 def absolute_loss(y, y_):
@@ -89,34 +105,10 @@ def absolute_loss(y, y_):
     """
     energy, energy_ = y[::,0::3], y_[::,0::3]
     theta, theta_ = y[::,1::3], y_[::,1::3]
-    phi, phi_ = y[::,2::3], y[::,2::3]
+    phi, phi_ = y[::,2::3], y_[::,2::3]
     
     loss_energy = LAMBDA_ENERGY*K.square(energy-energy_)
     loss_theta = LAMBDA_THETA*K.square(theta-theta_)
     loss_phi = LAMBDA_PHI*K.square(tf.math.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)
     return K.mean(loss_energy + loss_theta + loss_phi)
-
-  
-def vector_loss(y, y_):
-     
-     """ 
-
-     vektorbaserad kostnadsfunktion, förmodar att indata är i samma format som 2019 (energi, vinklar)
-     """
-
-     
-     energy, energy_ = y[::,0::3], y_[::,0::3]
-     theta, theta_ = y[::,1::3], y_[::,1::3]
-     phi, phi_ = y[::,2::3], y_[::,2::3]
-     
-     x,x_ = tf.math.multiply(tf.math.sin(theta), tf.math.cos(phi)), tf.math.multiply(tf.math.sin(theta_), tf.math.cos(phi_))
-     y,y_ = tf.math.multiply(tf.math.sin(theta), tf.math.sin(phi)), tf.math.multiply(tf.math.sin(theta_), tf.math.sin(phi_))
-     z,z_ = tf.math.cos(theta), tf.math.cos(theta_)
-     
-     r = [x,y,z]
-     r_ = [x_,y_,z_]
-     
-     loss_energy = LAMBDA_ENERGY*K.square(energy-energy_)
-     loss_angles = LAMBDA_VECTOR*(1-tf.math.multiply(x,x_)-tf.math.multiply(y,y_)-tf.math.multiply(z,z_))
-     
-     return K.mean(loss_energy + loss_angles)
+    
