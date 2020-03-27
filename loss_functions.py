fb4c330bc3c96e680ed8b3791640e6f5cd82ebf9
PajterMclovin <pajter_mclovin@hotmail.com>
2020-03-27 12:54:41 +0100

    includes if statements

--- loss_functions.py
+++ loss_functions.py
@@ -19,9 +19,16 @@ OFFSET_ENERGY = 0.1     #don't divide with 0! used in relative loss
 LAMBDA_VECTOR = 1       #used in vector loss
 
 
-def permutation_loss_wrapper(max_mult):
+def permutation_loss_wrapper(max_mult, loss_type='mean_squared_error'):
     """
-    Wrapper function for permutation_loss
+    Wrapper function for permutation_loss. Types of loss functions is:
+        mean_squared_error : standard
+        modulo : implementation of last years loss function
+        vector : Davids funky loss
+        cosine : uses a cosine term instead of modulo for periodicity
+    Args:
+        max_mult : maximum multiplicity
+        loss_type : what kind off loss function to use
     
     """
     if max_mult >= 7:
@@ -29,30 +36,51 @@ def permutation_loss_wrapper(max_mult):
     permutation_tensor = get_permutation_tensor(max_mult)
     identity_tensor = get_identity_tensor(max_mult)
     def loss(y, y_):
-        return permutation_loss(y, y_, permutation_tensor, identity_tensor)
+        return permutation_loss(y, y_, permutation_tensor, identity_tensor, loss_type)
     return loss
 
 
-def permutation_loss(y, y_, permutation_tensor, identity_tensor):
+def permutation_loss(y, y_, permutation_tensor, identity_tensor, loss_type):
     """
     A loss function that enables training without orderes labels (in fact, it 
-    requires no such clear order, e.g. decreasing energy).
+    requires no such clear order, e.g. decreasing energy). Used via the
+    wrapper function permutation_loss_wrapper above.
     
-         Args:
-        y : predicted data from network
-        y_ : corresponding labels (IMPORTANT: not ordered!)
-     Returns:
-        loss
     """
-    
     y_ = tf.transpose(K.dot(y_, permutation_tensor), perm=[1,0,2])
     y = tf.transpose(K.dot(y, identity_tensor), perm=[1,0,2])
     
-    loss_energy = LAMBDA_ENERGY*(K.square(y[::,::,0::3] - y_[::,::,0::3]))
-    loss_theta = LAMBDA_THETA*(K.square(y[::,::,1::3] - y_[::,::,1::3]))
-    loss_phi = LAMBDA_PHI*(K.square(y[::,::,2::3] - y_[::,::,2::3]))
+    energy, energy_ = y[::,::,0::3], y_[::,::,0::3]
+    theta, theta_ = y[::,::,1::3], y_[::,::,1::3]
+    phi, phi_ = y[::,::,2::3], y_[::,::,2::3]
+    
+    if loss_type=='mean_squared_error':
+        loss_energy = LAMBDA_ENERGY*K.square(energy - energy_)
+        loss_theta = LAMBDA_THETA*K.square(theta - theta_)
+        loss_phi = LAMBDA_PHI*K.square(phi - phi_)
+        
+    elif loss_type=='modulo':
+        loss_energy = LAMBDA_ENERGY*K.square(tf.divide(energy - energy_, energy_ + OFFSET_ENERGY))
+        loss_theta = LAMBDA_THETA*K.square(theta-theta_)
+        loss_phi = LAMBDA_PHI*K.square(tf.math.mod(phi - phi_ + np.pi, 2*np.pi) - np.pi)        
+        
+    elif loss_type=='vector':
+        x,x_ = tf.math.multiply(tf.math.sin(theta), tf.math.cos(phi)), tf.math.multiply(tf.math.sin(theta_), tf.math.cos(phi_))
+        y,y_ = tf.math.multiply(tf.math.sin(theta), tf.math.sin(phi)), tf.math.multiply(tf.math.sin(theta_), tf.math.sin(phi_))
+        z,z_ = tf.math.cos(theta), tf.math.cos(theta_)
+
+        loss_energy = LAMBDA_ENERGY*K.square(energy-energy_)
+        loss_theta = LAMBDA_VECTOR*(1-tf.math.multiply(x,x_)-tf.math.multiply(y,y_)-tf.math.multiply(z,z_))
+        loss_phi = 0
+        
+    elif loss_type=='cosine':
+        loss_energy = LAMBDA_ENERGY*K.square(energy - energy_)
+        loss_theta = LAMBDA_THETA*(1-K.cos(theta-theta_))
+        loss_phi = LAMBDA_PHI*(1-K.cos(phi-phi_))
+        
     
     return K.mean(K.min(K.sum(loss_energy+loss_theta+loss_phi, axis=2), axis=0))
+
 
 
 def relative_loss(y, y_):
