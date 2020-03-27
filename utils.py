fd73bfbd6f48799a02bc792e44e090fbc394119a
PajterMclovin <pajter_mclovin@hotmail.com>
2020-03-27 09:49:37 +0100

    moved plot methods

--- utils.py
+++ utils.py
@@ -1,15 +1,18 @@
 """ @PETER HALLDESTAM, 2020
+
     Help methods used in neural_network.py
     
+
 """
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras import layers
-from tensorflow.keras.backend import variable
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
+from tensorflow.keras import backend as K
 from itertools import permutations
 from math import factorial
+
+from loss_functions import LAMBDA_ENERGY, LAMBDA_THETA, LAMBDA_PHI
+
 
 def load_data(npz_file_name, total_portion):
     """
@@ -32,6 +35,7 @@ def load_data(npz_file_name, total_portion):
     no_events = int(len(labels)*total_portion)
     print('Using {} simulated events in total.'.format(no_events))
     return det_data[:no_events], labels[:no_events]
+
 
 def get_eval_data(data, labels, eval_portion):
     """
@@ -76,6 +80,70 @@ def sort_data(old_npz, new_npz):
     np.savez(new_npz, detector_data=data_set['detector_data'], energy_labels=sorted_labels)
     return sorted_labels
 
+
+def get_permutation_tensor(n):
+    """
+    Returns a tensor containing all n! permutation matrices. Dimension: [n!, 3n, 3n]
+    """
+    permutation_tensor = np.zeros((factorial(n), 3*n, 3*n))
+    depth = 0
+    for perm in permutations(range(n)):    
+        for i in range(n):
+            permutation_tensor[depth, 3*i:3*i+3:, 3*perm[i]:3*perm[i]+3:] = np.identity(3)
+        depth += 1
+    return K.variable(permutation_tensor)
+
+
+def get_identity_tensor(n):
+    """
+    Returns a tensor containing n! identity matrices. Dimension: [n!, 3n, 3n]
+    """
+    identity_tensor = np.zeros((factorial(n), 3*n, 3*n))
+    for depth in range(factorial(n)):
+            for i in range(n):
+                identity_tensor[depth, 3*i:3*i+3, 3*i:3*i+3] = np.identity(3)
+    return K.variable(identity_tensor)
+
+
+def get_permutation_match(y, y_):
+    """
+    Sorts the predictions with corresponding label as the minimum of a square 
+    error loss function. Must be used BEFORE plotting the "lasersvärd".
+    
+    Args:
+        y : use model.predict(data)
+        y_ : to compare with predictions y and sort
+    Returns:
+        y : same as input y
+        y_ : sorted labels
+    """
+    max_mult = int(len(y_[0])/3)
+    
+    def get_matching_label(x, x_):
+        min_loss = np.inf
+        for p in permutations(range(max_mult), max_mult):
+            perm_loss = 0
+            for i in range(max_mult):
+                energy_loss = LAMBDA_ENERGY*np.power(x[3*i] - x_[3*p[i]], 2)
+                theta_loss = LAMBDA_THETA*np.power(np.mod(x[3*i+1], 2*np.pi) - x_[3*p[i]+1], 2)
+                phi_loss = LAMBDA_PHI*np.power(np.mod(x[3*i+2], 2*np.pi) - x_[3*p[i]+2], 2)
+                perm_loss += energy_loss + theta_loss + phi_loss
+            if perm_loss < min_loss:
+                min_loss = perm_loss
+                perm = p
+        permutation_matrix = np.zeros((3*max_mult, 3*max_mult))
+        for i in range(max_mult):
+            permutation_matrix[3*i:3*i+3:, 3*perm[i]:3*perm[i]+3:] = np.identity(3)
+        return np.dot(x_, permutation_matrix)
+    
+    print('Matching predicted data with correct label permutation. May take a while...')
+    for i in range(len(y_)):
+        y_[i] = get_matching_label(y[i], y_[i])
+    print('done!')
+    return y, y_
+
+### ---------------- Davids code ----------------------------------------------
+
 def data_to_cartesian(old_npz, new_npz):
     """
     Converts the energy_labels of the of the .npz-file to cartesian coordinates
@@ -86,162 +154,34 @@ def data_to_cartesian(old_npz, new_npz):
         file whose energy labels to convert to cartesian coordinates
     new_npz : string
         file name for new npz-file
-
     Returns
     -------
     None.
-
     """
     data_set = np.load(old_npz)
     labels = data_set['energy_labels']
-    
+
     max_mult = int(len(labels[0])/3)
-    
+
     theta = labels[::, 1::3]
     phi = labels[::, 2::3]
-    
+
     x = tf.math.multiply(tf.math.sin(theta), tf.math.cos(phi))
     y = tf.math.multiply(tf.math.sin(theta), tf.math.sin(phi))
     z = tf.math.cos(theta)
-    
+
     cart_labels = np.zeros([len(labels), 4*max_mult])
-    
+
     cart_labels[::, 0::4] = labels[::, 0::3]
     cart_labels[::, 1::4] = x
     cart_labels[::, 2::4] = y
     cart_labels[::, 3::4] = z
-    
+
     np.savez(new_npz, detector_data=data_set['detector_data'], energy_labels=cart_labels)
     return cart_labels
+
     
-
-def plot_predictions(prediction, labels, bins=500):
-    """
-    Use to plot a models predictions in similar format as previous years, i.e. 2d histograms ("lasersvärd")
     
-    Args:
-        prediction : use model.predict(data)
-        labels : to compare with predictions
-    Returns:
-        figure, axes, events (dict)
-    Raises : if data and labels is not the same length
-        ValueError : if prediction and labels is not of same length
-    """
-    if not len(prediction)==len(labels):
-        raise TypeError('The prediction must be of same length as labels.') 
-        
-    events = {'predicted_energy': prediction[::,0::3].flatten(),
-              'correct_energy': labels[::,0::3].flatten(), 
-              
-              'predicted_theta': np.mod(prediction[::,1::3],np.pi).flatten(),
-              'correct_theta': labels[::,1::3].flatten(),
-              
-              'predicted_phi': np.mod(prediction[::,2::3],2*np.pi).flatten(),
-              'correct_phi': labels[::,2::3].flatten()}
-    
-    figure, axes = plt.subplots(1,3, figsize=(13, 5))
-    image = []
-    image.append(axes[0].hist2d(events['correct_energy'], events['predicted_energy'], bins=bins, norm=LogNorm(), cmax = 1001))
-    image.append(axes[1].hist2d(events['correct_theta'], events['predicted_theta'], bins=bins, norm=LogNorm(), cmax = 1001))
-    image.append(axes[2].hist2d(events['correct_phi'], events['predicted_phi'], bins=bins, norm=LogNorm(), cmax = 1001))
-    figure.tight_layout()
-    return figure, axes, events
-
-def plot_predictions_cart(prediction, labels, bins=500):
-    """
-    Use to plot a models predictions, with cartesian coordinates
-    
-    Args:
-        prediction : use model.predict(data)
-        labels : to compare with predictions
-    Returns:
-        figure, axes, events (dict)
-    Raises : if data and labels is not the same length
-        ValueError : if prediction and labels is not of same length
-    """
-    if not len(prediction)==len(labels):
-        raise TypeError('The prediction must be of same length as labels.') 
-        
-    events = {'predicted_energy': prediction[::,0::4].flatten(),
-              'correct_energy': labels[::,0::4].flatten(), 
-              
-              'predicted_x': prediction[::,1::4].flatten(),
-              'correct_x': labels[::,1::4].flatten(),
-              
-              'predicted_y': prediction[::,2::4].flatten(),
-              'correct_y': labels[::,2::4].flatten(),
-              
-              'predicted_z': prediction[::,3::4].flatten(),
-              'correct_z': labels[::,3::4].flatten()}
-    
-    figure, axes = plt.subplots(1,4, figsize=(13, 5))
-    image = []
-    image.append(axes[0].hist2d(events['correct_energy'], events['predicted_energy'], bins=bins, norm=LogNorm(), cmax = 1001))
-    image.append(axes[1].hist2d(events['correct_x'], events['predicted_x'], bins=bins, norm=LogNorm(), cmax = 1001))
-    image.append(axes[2].hist2d(events['correct_y'], events['predicted_y'], bins=bins, norm=LogNorm(), cmax = 1001))
-    image.append(axes[3].hist2d(events['correct_z'], events['predicted_z'], bins=bins, norm=LogNorm(), cmax = 1001))
-    figure.tight_layout()
-    return figure, axes, events
-    
-
-
-def plot_accuracy(history):
-    """
-    Learning curve; plot training and validation ACCURACY from model history
-    
-    Args:
-        history : tf.keras.callbacks.History object returned from model.fit
-    Returns:
-        -
-    Raises:
-        TypeError : if history is not History object
-    """
-    if not isinstance(history, tf.keras.callbacks.History):
-        raise TypeError('history must of type tf.keras.callbacks.History')
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'val'], loc='upper left')
-
-def plot_loss(history):
-    """
-    Learning curve; plot training and validation LOSS from model history
-    
-    Args:
-        history : tf.keras.callbacks.History object returned from model.fit
-    Returns:
-        -
-    Raises:
-        TypeError : if history is not History object
-    """
-    if not isinstance(history, tf.keras.callbacks.History):
-        raise TypeError('history must of type tf.keras.callbacks.History')
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'val'], loc='upper left')
-
-
-def get_permutation_tensor(n):
-    P = np.zeros((factorial(n), 3*n, 3*n))
-    depth = 0
-    for perm in permutations(range(n)):    
-        for i in range(n):
-            P[depth, 3*i:3*i+3:, 3*perm[i]:3*perm[i]+3:] = np.identity(3)
-        depth += 1
-    return variable(P)
-
-def get_identity_tensor(n):
-    I = np.zeros((factorial(n), 3*n, 3*n))
-    for depth in range(factorial(n)):
-            for i in range(n):
-                I[depth, 3*i:3*i+3, 3*i:3*i+3] = np.identity(3)
-    return variable(I)
-
 ### ----------------------------- INSPIRATION ---------------------------------
     
 #Need custom conv-Layer or some other solution before we can implement ResNet. BatchNorm is easy to implement!
