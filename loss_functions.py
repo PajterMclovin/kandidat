14a98542d3305c9c50ddbf2dabb6d8c3e66cbbec
PajterMclovin <pajter_mclovin@hotmail.com>
2020-04-07 11:17:25 +0200

    bör funka

--- loss_functions.py
+++ loss_functions.py
@@ -24,7 +24,7 @@ LAMBDA_CLASSIFICATION = 1 #used in binary cross entropy
 
 
 def loss_function_wrapper(no_outputs, loss_type='mse', permutation=True, 
-                          cartesian_coordinates=False, classification_nodes=False):
+                          cartesian=False, classification=False):
     """
     Wrapper function for permutation_loss and non_permutation_loss. Available 
     types of loss functions is:
@@ -41,14 +41,18 @@ def loss_function_wrapper(no_outputs, loss_type='mse', permutation=True,
     Returns:
         custom keras loss function
     """    
+    shift_tensor = False
     no_param = 3
-    if cartesian_coordinates:
-        print('Training with cartesian coordinate')
-        no_param += 1   #(theta, phi) => (x,y,z)
+    
+    if cartesian:
+        print('Training with cartesian coordinates (px, py, pz)')
+    else:
+        print('Training with spherical coordinates (energy, theta, phi)')
         
-    if classification_nodes:
+    if classification:
         print('Training with classification nodes')
         no_param += 1   #mu
+        shift_tensor = get_shift_tensor(int(no_outputs/no_param), m=no_param)
     
     max_mult = int(no_outputs/no_param)
     if max_mult >= 7:
@@ -58,25 +62,23 @@ def loss_function_wrapper(no_outputs, loss_type='mse', permutation=True,
         print('Using a permutation loss function of type ' + loss_type)
         permutation_tensor = get_permutation_tensor(max_mult, m=no_param)
         identity_tensor = get_identity_tensor(max_mult, m=no_param)
-        shift_tensor = get_shift_tensor(max_mult, m=no_param)           #for classification
-        return lambda y, y_: permutation_loss(y, y_, loss_type, permutation_tensor, 
-                                              identity_tensor, max_mult,
-                                              cartesian_coordinates=cartesian_coordinates,
-                                              classification_nodes=classification_nodes,
+        return lambda y, y_: permutation_loss(y, y_, loss_type, 
+                                              permutation_tensor, 
+                                              identity_tensor, 
+                                              cartesian=cartesian,
+                                              classification=classification,
                                               shift_tensor=shift_tensor)
     
     if not permutation:
         print('Using a non-permutation loss function of type: ' + loss_type)
         print('--> Make sure you train on an ordered label set!')
-        return lambda y, y_: non_permutation_loss(y, y_, loss_type,
-                                                  cartesian_coordinates=cartesian_coordinates,
-                                                  classification_nodes=classification_nodes)
-
+        return lambda y, y_: non_permutation_loss(y, y_, loss_type, 
+                                                  cartesian=cartesian,
+                                                  classification=classification)
 
 
 def permutation_loss(y, y_, loss_type, permutation_tensor, identity_tensor,
-                     max_mult, cartesian_coordinates=False,
-                     classification_nodes=False, shift_tensor=False):
+                     cartesian=False, classification=False, shift_tensor=False):
     """
     Loss functions that enable training without an ordered label set. Used via
     the wrapper function permutation_loss_wrapper above. It calculates the loss
@@ -88,7 +90,7 @@ def permutation_loss(y, y_, loss_type, permutation_tensor, identity_tensor,
 
     """
     
-    if classification_nodes:
+    if classification:
         y_ = K.dot(y, shift_tensor)
     
     #get all possible combinations
@@ -96,13 +98,12 @@ def permutation_loss(y, y_, loss_type, permutation_tensor, identity_tensor,
     y = tf.transpose(K.dot(y, identity_tensor), perm=[1,0,2])
     
     #what loss functions to minimize
-    loss = Loss_function(loss_type, cartesian_coordinates=cartesian_coordinates,
-                         classification_nodes=classification_nodes).loss(y, y_)
+    loss = Loss_function(loss_type, cartesian=cartesian,classification=classification).loss(y, y_)
     return K.mean(K.min(K.sum(loss, axis=2), axis=0))
 
 
-def non_permutation_loss(y, y_, loss_type, cartesian_coordinates=False,
-                     classification_nodes=False, shift_tensor=False):
+def non_permutation_loss(y, y_, loss_type, cartesian=False,
+                     classification=False, shift_tensor=False):
     """
     Loss functions trained with an ordered label set and does not check every
     permutation.
@@ -112,8 +113,7 @@ def non_permutation_loss(y, y_, loss_type, cartesian_coordinates=False,
     y = K.expand_dims(y, axis=0)
     y_ = K.expand_dims(y_, axis=0)
     
-    loss = Loss_function(loss_type, cartesian_coordinates=cartesian_coordinates,
-                         classification_nodes=classification_nodes).loss(y, y_)
+    loss = Loss_function(loss_type, cartesian=cartesian, classification=classification).loss(y, y_)
     return K.mean(K.sum(loss, axis=1))
 
 
@@ -121,12 +121,12 @@ def non_permutation_loss(y, y_, loss_type, cartesian_coordinates=False,
 
 
 class Loss_function(object):
-    def __init__(self, loss_type, cartesian_coordinates=False, classification_nodes=False):
+    def __init__(self, loss_type, cartesian=False, classification=False):
         self.loss = False
         self.loss_type = loss_type
-        self.cartesian_coordinates = cartesian_coordinates
+        self.cartesian = cartesian
         
-        if classification_nodes:
+        if classification:
             self.loss = self.loss_with_binary_cross_entropy
         else:
             self.loss = self.get_loss_type()
@@ -134,12 +134,11 @@ class Loss_function(object):
             raise ValueError('invalid loss function type')
             
     def get_loss_type(self):
-        if self.cartesian_coordinates:
-            if self.loss_type=='mse': return self.cartesian_mse_loss
+        if self.cartesian:
+            return self.momentum_loss
         else:
             if self.loss_type=='mse':  return self.mse_loss
             elif self.loss_type=='modulo': return self.modulo_loss
-            elif self.loss_type=='vector': return self.davids_vector_loss
             elif self.loss_type=='cosine': return self.cosine_loss
             
             
@@ -155,19 +154,7 @@ class Loss_function(object):
         loss_theta = LAMBDA_THETA*K.square(y[::,::,1::3] - y_[::,::,1::3])
         loss_phi = LAMBDA_PHI*K.square(tf.math.mod(y[::,::,2::3] - y_[::,::,2::3] + np.pi, 2*np.pi) - np.pi)       
         return loss_energy+loss_theta+loss_phi
-    
-    def davids_vector_loss(self, y, y_):
-        x = tf.math.multiply(tf.math.sin(y[::,::,1::3]), tf.math.cos(y[::,::,2::3])) 
-        y = tf.math.multiply(tf.math.sin(y[::,::,1::3]), tf.math.sin(y[::,::,2::3]))
-        z = tf.math.cos(y[::,::,1::3])
-        
-        x_ =  tf.math.multiply(tf.math.sin(y_[::,::,1::3]), tf.math.cos(y_[::,::,2::3]))
-        y_ = tf.math.multiply(tf.math.sin(y_[::,::,1::3]), tf.math.sin(y_[::,::,2::3]))
-        z_ = tf.math.cos(y_[::,::,1::3])
-        
-        loss_energy = LAMBDA_ENERGY*K.square(y[::,::,0::3] - y_[::,::,0::3])
-        loss_vector = LAMBDA_VECTOR*(1-tf.math.multiply(x,x_)-tf.math.multiply(y,y_)-tf.math.multiply(z,z_))
-        return loss_energy+loss_vector
+
     
     def cosine_loss(self, y, y_):
         loss_energy = LAMBDA_ENERGY*K.square(y[::,::,0::3] - y_[::,::,0::3])
@@ -177,16 +164,12 @@ class Loss_function(object):
     
     ## --------------- CARTESIAN COORDINATE LOSS FUNCTIONS --------------------
     
-    def cartesian_mse_loss(self, y, y_):
-        loss_energy = LAMBDA_ENERGY*K.square(y[::,::,0::4]-y_[::,::,0::4])        
-        X, X_ = y[::,::,1::4], y_[::,::,1::4]
-        Y, Y_ = y[::,::,2::4], y_[::,::,2::4]
-        Z, Z_ = y[::,::,3::4], y_[::,::,3::4]
-        loss_X = K.square(X-X_)
-        loss_Y = K.square(Y-Y_)
-        loss_Z = K.square(Z-Z_)
-        loss_R = K.square(1-X*X-Y*Y-Z*Z)        
-        return LAMBDA_ENERGY*loss_energy + LAMBDA_XYZ*(loss_X + loss_Y + loss_Z) + loss_R
+    def momentum_loss(self, y, y_):
+        px, px_ = y[...,0::3], y_[...,0::3]
+        py, py_ = y[...,1::3], y_[...,1::3]
+        pz, pz_ = y[...,2::3], y_[...,2::3]
+        return K.square(px-px_) + K.square(py-py_) + K.square(pz-pz_)
+
 
     ## ----------------- BINARY CLASSIFICATION LOSS FUNCTIONS -----------------
     
