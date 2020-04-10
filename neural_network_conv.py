ef47c407ec0899814a77d971017f7cc2e97888c5
DavidRinman <61792198+DavidRinman@users.noreply.github.com>
2020-04-10 17:42:48 +0200

    fixat

--- neural_network_conv.py
+++ neural_network_conv.py
@@ -6,38 +6,47 @@
 """
 
 from tensorflow.keras.optimizers import Adam
-from time import time
-import h5py
 
-import numpy as np
-
-from models import CNN, FCN
+from models import FCN, GCN, CNN
 from loss_functions import loss_function_wrapper
-from utils import load_data, get_eval_data
 from plotting import plot_predictions
+from plotting import plot_loss
+from utils import load_data
+from utils import save
+from utils import get_eval_data
+from utils import get_permutation_match
+from utils import cartesian_to_spherical
+from utils import get_no_trainable_parameters
 
 ## ----------------------------- PARAMETERS -----------------------------------
 
-NPZ_DATAFILE = 'test.npz'   #or import sys and use sys.argv[1]
-TOTAL_PORTION = 0.3                             #portion of file data to be used, (0,1]
+NPZ_DATAFILE = 'test.npz'                        #or import sys and use sys.argv[1]
+TOTAL_PORTION = 0.5                          #portion of file data to be used, (0,1]
 EVAL_PORTION = 0.1                              #portion of total data for final evalutation (0,1)
 VALIDATION_SPLIT = 0.1                          #portion of training data for epoch validation
-CARTESIAN = False                                #train with cartesian coordinates instead of spherical
+CARTESIAN = True                                #train with cartesian coordinates instead of spherical
+CLASSIFICATION = False                          #train with classification nodes
 
-NO_EPOCHS = 1                                   #Number of times to go through training data
-BATCH_SIZE = 256                                #The training batch size
+NO_EPOCHS = 20
+                                   #Number of times to go through training data
+BATCH_SIZE = 2**8                                #The training batch size
 LEARNING_RATE = 1e-4                            #Learning rate/step size
-PERMUTATION = False                              #set false if using an ordered data set
-LOSS_FUNCTION = 'mse'                        #type of loss: {mse, modulo, vector, cosine}
+PERMUTATION = True                              #set false if using an ordered data set
+LOSS_FUNCTION = 'mse'                           #type of loss: {mse, modulo, cosine} (only mse for cartesian)
 
 
 def main():
     #load simulation data. OBS. labels need to be ordered in decreasing energy!
-    data, labels = load_data(NPZ_DATAFILE, TOTAL_PORTION, cartesian_coordinates=CARTESIAN)
+    data, labels = load_data(NPZ_DATAFILE, TOTAL_PORTION, 
+                             cartesian=CARTESIAN,
+                             classification=CLASSIFICATION)
     
     #detach subset for final evaluation. train_** is for both training and validation
     train_data, train_labels, eval_data, eval_labels = get_eval_data(data, labels,
                                                                      eval_portion=EVAL_PORTION)
+    
+    
+    ### ------------- BUILD, TRAIN & TEST THE NEURAL NETWORK ------------------
     
     
     ### ------------- BUILD, TRAIN & TEST THE NEURAL NETWORK ------------------
@@ -50,10 +59,11 @@ def main():
     model = CNN(no_inputs, no_outputs)
     
     #select loss function
-    loss_function = loss_function_wrapper(int(no_outputs/3), 
+    loss_function = loss_function_wrapper(no_outputs, 
                                           loss_type=LOSS_FUNCTION, 
                                           permutation=PERMUTATION,
-                                          cartesian_coordinates=CARTESIAN)
+                                          cartesian=CARTESIAN,
+                                          classification=CLASSIFICATION)
     
     #select optimizer
     opt = Adam(lr=LEARNING_RATE)
@@ -62,15 +72,39 @@ def main():
     model.compile(optimizer=opt, loss=loss_function, metrics=['accuracy'])
     model.summary()
    
-    
-    #train the network with training data
     training = model.fit(train_data, train_labels, 
                          epochs=NO_EPOCHS, batch_size=BATCH_SIZE,
                          validation_split=VALIDATION_SPLIT)
     
+    #plot the learning curve
+    learning_curve = plot_loss(training)
+     
+    
     #plot predictions on evaluation data
     predictions = model.predict(eval_data)
-    figure, axes, rec_events = plot_predictions(predictions, eval_labels, permutation=False)
+
+    
+    if CARTESIAN:
+        predictions = cartesian_to_spherical(predictions)
+        eval_labels = cartesian_to_spherical(eval_labels)    
+    if PERMUTATION:
+        predictions, labels = get_permutation_match(predictions, eval_labels, CARTESIAN, loss_type=LOSS_FUNCTION)
+
+    
+    
+    #plot the "lasersvärd"
+    figure, rec_events = plot_predictions(predictions, eval_labels, 
+                                                show_detector_angles=True)
+    
+    #add title
+    no_params = get_no_trainable_parameters(model)
+    title = """trainable parameters: {}, epochs: {}, loss: {}, 
+               cartesian: {}, permutation: {}, max_mult: {},
+               #events: {} (training) {} (evaluation, shown)
+            """.format(no_params, NO_EPOCHS, LOSS_FUNCTION, 
+                       CARTESIAN, PERMUTATION, int(no_outputs/3),
+                       len(train_data), len(eval_data))
+    figure.suptitle(title)
     
     
     
