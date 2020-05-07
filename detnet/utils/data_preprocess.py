19912ecbc10a7139833cf7483c745ba751d102d6
PajterMclovin <pajter_mclovin@hotmail.com>
2020-05-07 13:53:23 +0200

    Update data_preprocess.py

--- detnet/utils/data_preprocess.py
+++ detnet/utils/data_preprocess.py
@@ -4,73 +4,90 @@
     
 """
 import numpy as np
-from random import shuffle
+from numpy.random import permutation
+from random import sample
 
-from utils.help_methods import spherical_to_cartesian
+from .help_methods import spherical_to_cartesian
 
 
-def load_data(npz_file_name, total_portion, 
-              cartesian=False, classification=False):
+def load_data(npz_file, total_portion, add_zeros=0, classification=False, hinge=False):
     """
-    Reads a .npz-file containing simulation data in spherical coordinates and
-    returns data/labels in spherical or cartesian coordinates in numpy arrays
+    Reads a .npz-file from the address string: npz_file that contains simulation 
+    data in spherical coordinates. I transforms to cartesian coordinates and
+    may add n empty events and also add classification nodes.
     
-    Args:
-        npz_file_name : address string to .npz-file
-        total_portion : the actual amount of total data to be used
-        cartesian_coordinates : set True for cartesian coordinates
-        classification_nodes : set True to train with classification nodes
-    Returns:
-        all data and labels in spherical (or cartesian) coordinates
-    Raises:
-        ValueError : if portion is not properly set
     """
     if not total_portion > 0 and total_portion <= 1:
         raise ValueError('total_portion must be in the interval (0,1].')
-    data_set = np.load(npz_file_name)
+        
+    data_set = np.load(npz_file)
     det_data = data_set['detector_data']
-    labels = data_set['energy_labels']
-    if cartesian:
-        labels = spherical_to_cartesian(labels)
+    labels = spherical_to_cartesian(data_set['energy_labels'])
+    
+    if add_zeros:
+        det_data, labels = insert_empty_events(det_data, labels, n=add_zeros)
+    
     if classification:
-        labels = insert_classification_labels(labels, cartesian=cartesian)
+        labels = insert_classification_labels(labels, hinge)
+    
     no_events = int(len(labels)*total_portion)
-    print('Using {} events from {}'.format(no_events, npz_file_name))
+    print('Using {} events from {}'.format(no_events, npz_file))
     return det_data[:no_events], labels[:no_events]
 
 
-def insert_classification_labels(labels, cartesian=False):
+def insert_empty_events(data, labels, n):
     """
-    Inserts binary classification labels for each event as:
-        energy==0 => mu=1
-        energy >0 => mu=0
+    Inserts n empty events into given data and label set.
+    
     """
-    m = 3 + cartesian
-    energy = labels[::,0::m]
-    pos = labels[::,[i for i in range(len(labels[0])) if np.mod(i,m)!=0]]
-    mu = (energy!=0)*1
-    print(len(mu))
-    max_mult = int(len(labels[0])/m)
-    new_labels = np.zeros((len(labels), max_mult*(m+1)))
-    new_labels[::,0::m+1] = mu
-    new_labels[::,1::m+1] = energy
-    new_labels[::,[i for i in range(len(labels[0])+max_mult) if np.mod(i,m+1)!=0 and np.mod(i-1,m+1)!=0]]=pos
-    return new_labels
+    if n > 10*len(labels):
+        raise ValueError('Too many zeros to add...')
     
+    print('inserting {} zero events...'.format(n))
+    new_data = data
+    new_labels = labels
+    no_insertions = n
     
+    empty_data = np.zeros(data[0].shape)
+    empty_label = np.zeros(labels[0].shape)
+    
+    while no_insertions > len(new_labels):
+        insert_indices = permutation(len(new_labels))
+        new_data = np.insert(new_data, insert_indices, empty_data, axis=0)
+        new_labels = np.insert(new_labels, insert_indices, empty_label, axis=0)
+        no_insertions -= len(insert_indices)
+    
+    insert_indices = permutation(no_insertions)
+    new_data = np.insert(new_data, insert_indices, empty_data, axis=0)
+    new_labels = np.insert(new_labels, insert_indices, empty_label, axis=0)
+    return new_data, new_labels
+    
+
+def insert_classification_labels(labels, hinge):
+    """
+    Inserts binary classification labels (b,px,py,pz) for each event as:
+        energy==0 => b=1
+        energy>0 => b=0
+    """
+    px_ = labels[::,0::3].flatten()
+    py_ = labels[::,1::3].flatten()
+    pz_ = labels[::,2::3].flatten()
+    
+    energy = np.sqrt(px_*px_+py_*py_+pz_*pz_)
+    b_ = np.array((energy!=0)*1)
+    
+    if hinge:
+        b_ = 2*(b_ - .5)
+    
+    max_mult = int(len(labels[0])/3)
+    return np.reshape(np.column_stack((b_, px_, py_, pz_)), (-1, 4*max_mult))
+    
+ 
 def get_eval_data(data, labels, eval_portion=0.1):
     """
-    Detach final evaluation data.
-    
-    Args:
-        data : dataset to split in two
-        labels : corresponding data labels
-        eval_portion : the amount of data for final evaluation
-    Returns:
-        training/validation data and labels
-        final evaluation data and labels
-    Raises:
-        ValueError : if portion is not properly set
+    Seperates into data and labels set into one for training/validiation and
+    another for final evaluation.
+
     """
     if not eval_portion > 0 and eval_portion < 1:
         raise ValueError('eval_portion must be in interval (0,1).')
@@ -81,11 +98,8 @@ def get_eval_data(data, labels, eval_portion=0.1):
 def sort_data(old_npz, new_npz):
     """
     Sort the label data matrix with rows (energy1, theta1, phi1, ... phiM), 
-    where M is the max multiplicity.
-    
-    Args:
-        old_npz : address of .npz-file with label data to sort
-        new_npz : name of new .npz file
+    where M is the max multiplicity and saves to new npz file.
+
     """
     data_set = np.load(old_npz)
     labels = data_set['energy_labels']
@@ -99,27 +113,6 @@ def sort_data(old_npz, new_npz):
     np.savez(new_npz, detector_data=data_set['detector_data'], energy_labels=sorted_labels)
 
 
-def add_empty_events(old_npz, new_npz, n):
-    """
-    Appends n empty events into data/label, shuffles them and then saves it to
-    a new npz-file. Surely not the optimal solution but it should work. Maybe
-    fucks up with larger data sets...
     
-    Args:
-        old_npz : address of .npz-file with label data to sort
-        new_npz : name of new .npz file
-    """
-    data_set = np.load(old_npz)
-    data = data_set['detector_data']
-    labels = data_set['energy_labels']
     
-    no_inputs = len(data[0])
-    no_outputs = len(labels[0])
-    data_list = np.vstack([data, np.zeros((n, no_inputs))]).tolist()
-    label_list = np.vstack([labels, np.zeros((n, no_outputs))]).tolist()
-    
-    tmp = list(zip(data_list, label_list))
-    shuffle(tmp)
-    new_data, new_labels = zip(*tmp)
-    np.savez(new_npz, detector_data=new_data, energy_labels=new_labels)
     
